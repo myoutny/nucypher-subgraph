b741b9ea6b8955834af8e7c1e393b566fb3f21db
KPrasch <kieranprasch@gmail.com>
2021-04-23 15:14:09 -0700

    Modeling Periods and use Event params for index lookups.

--- src/StakingEscrow-mapping.ts
+++ src/StakingEscrow-mapping.ts
@@ -4,66 +4,123 @@ import {
     Divided,
     Locked,
     Merged,
+    Migrated,
     Minted,
     ReStakeSet,
+    Slashed,
+    SnapshotSet,
+    StakingEscrow,
     WindDownSet,
     Withdrawn,
     WorkerBonded
 } from "../generated/StakingEscrow/StakingEscrow"
-import {getOrCreateStaker} from "../utils/helpers";
+import {getOrCreateStaker, ZERO_BI} from "../utils/helpers";
+import {Period} from "../generated/schema";
 
 export function handleLocked(event: Locked): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.substakes = staker.substakes + BigInt.fromI32(1)
     staker.staked = staker.staked + event.params.value
     staker.save()
 }
 
 export function handleDivided(event: Divided): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.substakes = staker.substakes + BigInt.fromI32(1)
     staker.save()
 }
 
 export function handleMerged(event: Merged): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.substakes = staker.substakes - BigInt.fromI32(1)
     staker.save()
 }
 
 export function handleCommitmentMade(event: CommitmentMade): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
-    staker.commitment = BigInt.fromI32(event.params.period)
-    staker.save()
+    let staker = getOrCreateStaker(event.params.staker.toHex())
+
+    let next_period_number = event.params.period
+
+    if (next_period_number < 10000) {
+        // Weekly Periods (168)
+
+        let next_period = Period.load(next_period_number.toString())
+        if (next_period == null) {
+            next_period = new Period(next_period_number.toString())
+
+            // Initialize next period
+            next_period.timestamp = event.block.timestamp.toI32()
+            next_period.activeStakers = ZERO_BI
+            next_period.totalStaked = ZERO_BI
+            next_period.save()
+
+            // Finalize previous period circulating supply
+            let prev_period_number = BigInt.fromI32(next_period_number - 2).toString()
+            let previous_period = Period.load(prev_period_number.toString())
+            let contract = StakingEscrow.bind(event.address)
+            previous_period.circulatingSupply = contract.previousPeriodSupply()
+            previous_period.save()
+
+        }
+        next_period.totalStaked = next_period.totalStaked + event.params.value
+        next_period.activeStakers = next_period.activeStakers + BigInt.fromI32(1)
+        next_period.save()
+
+        staker.commitment = BigInt.fromI32(event.params.period)
+        staker.save()
+
+    } else {
+        // Daily Periods (24)
+    }
+
+
 }
 
 export function handleMinted(event: Minted): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.minted = staker.minted + event.params.value
     staker.save()
 }
 
 export function handleReStakeSet(event: ReStakeSet): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.restaking = event.params.reStake
     staker.save()
 }
 
+export function handleSnapshotSet(event: SnapshotSet): void {
+    let staker = getOrCreateStaker(event.params.staker.toHex())
+    staker.snapshots = event.params.snapshotsEnabled
+    staker.save()
+}
+
 export function handleWindDownSet(event: WindDownSet): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.winding_down = event.params.windDown
     staker.save()
 }
 
 export function handleWithdrawn(event: Withdrawn): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
-    staker.withdrawn = staker.withdrawn + event.transaction.value
+    let staker = getOrCreateStaker(event.params.staker.toHex())
+    staker.withdrawn = staker.withdrawn + event.params.value
     staker.save()
 }
 
 export function handleWorkerBonded(event: WorkerBonded): void {
-    let staker = getOrCreateStaker(event.transaction.from.toHex())
-    staker.bonded = true
+    let staker = getOrCreateStaker(event.params.staker.toHex())
     staker.worker = event.params.worker.toHex()
+    staker.bonded = true
+    staker.save()
+}
+
+export function handleMigrated(event: Migrated): void {
+    let staker = getOrCreateStaker(event.params.staker.toHex())
+    staker.migrated = true
+    staker.save()
+}
+
+export function handleSlashed(event: Slashed): void {
+    let staker = getOrCreateStaker(event.params.staker.toHex())
+    staker.slashed = staker.slashed + event.params.penalty
     staker.save()
 }
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -9,8 +9,41 @@ export let NULL_ADDRESS = Address.fromString(
 export let ZERO_BI = BigInt.fromI32(0);
 
 
+export function getNuCypherTokenAddress(network: string): string {
+  if (network == "mainnet") {
+    return "4fE83213D56308330EC302a8BD641f1d0113A4Cc";
+  } else if (network == "rinkeby") {
+    return "6A6F917a3FF3d33d26BB4743140F205486cD6B4B";
+  } else if (network == "goerli") {
+    return "02B50E38E5872068F325B1A7ca94D90ce2bfff63";
+  }
+}
+
+
+export function getStakingEscrowAddress(network: string): string {
+  if (network == "mainnet") {
+    return "bbD3C0C794F40c4f993B03F65343aCC6fcfCb2e2";
+  } else if (network == "rinkeby") {
+    return "6A6F917a3FF3d33d26BB4743140F205486cD6B4B";
+  } else if (network == "goerli") {
+    return "40Ca356d8180Ddc21C82263F9EbCeaAc6Cad7250";
+  }
+}
+
+
+export function getPolicyManagerAddress(network: string): string {
+  if (network == "mainnet") {
+    return "67E4A942c067Ff25cE7705B69C318cA2Dfa54D64";
+  } else if (network == "rinkeby") {
+    return "4db603E42E6798Ac534853AA2c0fF5618cb0ebE5";
+  } else if (network == "goerli") {
+    return "aC5e34d3FD41809873968c349d1194D23045b9D2";
+  }
+}
+
+
 export function getOrCreateStaker(id: string): Staker {
-  let staker = Staker.load(id);
+    let staker = Staker.load(id);
     if (staker == null) {
         staker = new Staker(id)
         staker.staked = ZERO_BI
@@ -22,6 +55,8 @@ export function getOrCreateStaker(id: string): Staker {
         staker.minted = ZERO_BI
         staker.withdrawn = ZERO_BI
         staker.commitment = ZERO_BI
+        staker.migrated = false
+        staker.save()
     }
   return staker as Staker;
 }
