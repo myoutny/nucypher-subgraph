de18add7c50b628610807ab8c9acb867db8d1a7d
Kieran R. Prasch <kieranprasch@gmail.com>
2021-04-29 12:04:55 -0700

    Index staker event records as entities

--- src/StakingEscrow-mapping.ts
+++ src/StakingEscrow-mapping.ts
@@ -15,72 +15,199 @@ import {
     Withdrawn,
     WorkerBonded
 } from "../generated/StakingEscrow/StakingEscrow"
-import {convertToDecimal, getOrCreateStaker, NULL_ADDRESS, ZERO_BD, ZERO_BI} from "../utils/helpers";
-import {Period} from "../generated/schema";
+import {
+    convertToDecimal,
+    createPeriod,
+    finalizePeriod,
+    getOrCreateStaker,
+    getOrCreateTransaction,
+    getPeriodNumber,
+    makeEventId,
+    NULL_ADDRESS
+} from "../utils/helpers";
+import {
+    CommitmentEvent,
+    DepositedEvent,
+    DividedEvent,
+    LockedEvent,
+    MergedEvent,
+    MigratedEvent,
+    MintedEvent,
+    Period,
+    ReStakeEvent,
+    SlashedEvent,
+    WindDownEvent,
+    WithdrawEvent,
+    WorkerBondedEvent
+} from "../generated/schema";
 
 
 export function handleLocked(event: Locked): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
     let contract = StakingEscrow.bind(event.address)
+    let lockedEvent = new LockedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // base
+    lockedEvent.transaction = event.transaction.hash.toHex()
+    lockedEvent.timestamp = event.block.timestamp.toI32()
+    lockedEvent.period = periodNumber.toString()
+    lockedEvent.staker = staker.id
+
+    // record
+    lockedEvent.value = convertToDecimal(event.params.value)
+    lockedEvent.firstPeriod = BigInt.fromI32(event.params.firstPeriod)
+    lockedEvent.duration = BigInt.fromI32(event.params.periods)
+    lockedEvent.save()
+
+    // staker
     staker.substakes = contract.getSubStakesLength(event.params.staker)
-    staker.staked = staker.staked + convertToDecimal(event.params.value)
+    staker.staked = staker.staked.plus(convertToDecimal(event.params.value))
     staker.save()
+
 }
 
 export function handleDivided(event: Divided): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
     let contract = StakingEscrow.bind(event.address)
+    let dividedEvent = new DividedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    dividedEvent.transaction = event.transaction.hash.toHex()
+    dividedEvent.timestamp = event.block.timestamp.toI32()
+    dividedEvent.period = periodNumber.toString()
+    dividedEvent.staker = staker.id
+
+    // event record
+    dividedEvent.oldValue = convertToDecimal(event.params.oldValue)
+    dividedEvent.newValue = convertToDecimal(event.params.newValue)
+    dividedEvent.lastPeriod = BigInt.fromI32(event.params.lastPeriod)
+    dividedEvent.duration = BigInt.fromI32(event.params.periods)
+    dividedEvent.save()
+
+    // staker
     staker.substakes = contract.getSubStakesLength(event.params.staker)
     staker.save()
 }
 
 export function handleMerged(event: Merged): void {
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
     let contract = StakingEscrow.bind(event.address)
+    let mergedEvent = new MergedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    mergedEvent.transaction = event.transaction.hash.toHex()
+    mergedEvent.timestamp = event.block.timestamp.toI32()
+    mergedEvent.period = periodNumber.toString()
+    mergedEvent.staker = staker.id
+
+    // event record
+    mergedEvent.value1 = convertToDecimal(event.params.value1)
+    mergedEvent.value2 = convertToDecimal(event.params.value2)
+    mergedEvent.lastPeriod = BigInt.fromI32(event.params.lastPeriod)
+    mergedEvent.save()
+
+    // staker
     staker.substakes = contract.getSubStakesLength(event.params.staker)
     staker.save()
 }
 
 export function handleCommitmentMade(event: CommitmentMade): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let currentPeriodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let commitmentEvent = new CommitmentEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    commitmentEvent.transaction = event.transaction.hash.toHex()
+    commitmentEvent.timestamp = event.block.timestamp.toI32()
+    commitmentEvent.period = currentPeriodNumber.toString()
+    commitmentEvent.staker = staker.id
+
+    // event record
+    commitmentEvent.commitmentPeriod = BigInt.fromI32(event.params.period)
+    commitmentEvent.save()
 
     let nextPeriodNumber = event.params.period
     let nextPeriod = Period.load(nextPeriodNumber.toString())
     if (nextPeriod == null) {
-        nextPeriod = new Period(nextPeriodNumber.toString())
-
-        // Initialize next period
-        nextPeriod.timestamp = event.block.timestamp.toI32()
-        nextPeriod.activeStakers = ZERO_BI
-        nextPeriod.totalStaked = ZERO_BD
-
-        // Finalize previous period circulating supply
-        let prevPeriodNumber = BigInt.fromI32(nextPeriodNumber - 2).toString()
-        let prevPeriod = Period.load(prevPeriodNumber.toString())
-        if (prevPeriod != null) {
-            let contract = StakingEscrow.bind(event.address)
-            prevPeriod.circulatingSupply = convertToDecimal(contract.previousPeriodSupply())
-            prevPeriod.save()
-        }
+        // Finalize previous period before creating the next one.
+        finalizePeriod(nextPeriodNumber - 2)
+        // Initialize the next period
+        nextPeriod = createPeriod(BigInt.fromI32(nextPeriodNumber), event.block.timestamp.toI32())
     }
 
-    // Accumulate
-    nextPeriod.totalStaked = nextPeriod.totalStaked + convertToDecimal(event.params.value)
-    nextPeriod.activeStakers = nextPeriod.activeStakers + BigInt.fromI32(1)
+    // Accumulate the next period's data
+    nextPeriod.totalStaked = nextPeriod.totalStaked.plus(convertToDecimal(event.params.value))
+    nextPeriod.activeStakers = nextPeriod.activeStakers.plus(BigInt.fromI32(1))
     nextPeriod.save()
 
-    staker.commitment = BigInt.fromI32(event.params.period)
+    // Update this staker's last commitment period
+    staker.lastCommitment = BigInt.fromI32(event.params.period)
     staker.save()
 }
 
 export function handleMinted(event: Minted): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
-    staker.minted = staker.minted + convertToDecimal(event.params.value)
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let mintedEvent = new MintedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    mintedEvent.transaction = event.transaction.hash.toHex()
+    mintedEvent.timestamp = event.block.timestamp.toI32()
+    mintedEvent.period = periodNumber.toString()
+    mintedEvent.staker = staker.id
+
+    // event record
+    mintedEvent.value = convertToDecimal(event.params.value)
+    mintedEvent.mintingPeriod = BigInt.fromI32(event.params.period)
+    mintedEvent.save()
+
+    // update previous period
+    let prevPeriod = Period.load(BigInt.fromI32(event.params.period).toString())
+    if (prevPeriod != null) {
+        prevPeriod.minted = prevPeriod.minted.plus(convertToDecimal(event.params.value))
+        prevPeriod.save()
+    }
+
+    // staker
+    staker.minted = staker.minted.plus(convertToDecimal(event.params.value))
     staker.save()
 }
 
 export function handleReStakeSet(event: ReStakeSet): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let reStakeEvent = new ReStakeEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    reStakeEvent.transaction = event.transaction.hash.toHex()
+    reStakeEvent.timestamp = event.block.timestamp.toI32()
+    reStakeEvent.period = periodNumber.toString()
+    reStakeEvent.staker = staker.id
+
+    // event record
+    reStakeEvent.reStake = event.params.reStake
+    reStakeEvent.save()
+
+    // staker
     staker.restaking = event.params.reStake
     staker.save()
 }
@@ -92,38 +219,141 @@ export function handleSnapshotSet(event: SnapshotSet): void {
 }
 
 export function handleWindDownSet(event: WindDownSet): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let windDownEvent = new WindDownEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // base
+    windDownEvent.transaction = event.transaction.hash.toHex()
+    windDownEvent.timestamp = event.block.timestamp.toI32()
+    windDownEvent.period = periodNumber.toString()
+    windDownEvent.staker = staker.id
+
+    // record
+    windDownEvent.windDown = event.params.windDown
+    windDownEvent.save()
+
+    // staker
     staker.windingDown = event.params.windDown
     staker.save()
 }
 
 export function handleWithdrawn(event: Withdrawn): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
-    staker.withdrawn = staker.withdrawn + convertToDecimal(event.params.value)
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let withdrawEvent = new WithdrawEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    withdrawEvent.transaction = event.transaction.hash.toHex()
+    withdrawEvent.timestamp = event.block.timestamp.toI32()
+    withdrawEvent.period = periodNumber.toString()
+    withdrawEvent.staker = staker.id
+
+    // event record
+    withdrawEvent.value = convertToDecimal(event.params.value)
+    withdrawEvent.save()
+
+    // staker
+    staker.withdrawn = staker.withdrawn.plus(convertToDecimal(event.params.value))
     staker.save()
 }
 
 export function handleWorkerBonded(event: WorkerBonded): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let workerBondedEvent = new WorkerBondedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    workerBondedEvent.transaction = event.transaction.hash.toHex()
+    workerBondedEvent.timestamp = event.block.timestamp.toI32()
+    workerBondedEvent.period = periodNumber.toString()
+    workerBondedEvent.staker = staker.id
+
+    // event record
+    workerBondedEvent.worker = event.params.worker.toHex()
+    workerBondedEvent.startPeriod = BigInt.fromI32(event.params.startPeriod)
+    workerBondedEvent.save()
+
+    // staker
     staker.worker = event.params.worker.toHex()
-    staker.bonded = staker.worker != NULL_ADDRESS.toHex();
+    staker.bonded = staker.worker != NULL_ADDRESS.toHex()
     staker.save()
 }
 
 export function handleMigrated(event: Migrated): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let migratedEvent = new MigratedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event record
+    migratedEvent.transaction = event.transaction.hash.toHex()
+    migratedEvent.timestamp = event.block.timestamp.toI32()
+    migratedEvent.period = periodNumber.toString()
+    migratedEvent.staker = staker.id
+    migratedEvent.save()
+
+    // staker
     staker.migrated = true
     staker.save()
 }
 
 export function handleSlashed(event: Slashed): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
-    staker.slashed = staker.slashed + convertToDecimal(event.params.penalty)
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let slashedEvent = new SlashedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    slashedEvent.transaction = event.transaction.hash.toHex()
+    slashedEvent.timestamp = event.block.timestamp.toI32()
+    slashedEvent.period = periodNumber.toString()
+    slashedEvent.staker = staker.id
+
+    // event record
+    slashedEvent.penalty = convertToDecimal(event.params.penalty)
+    slashedEvent.investigator = event.params.investigator.toString()
+    slashedEvent.reward = convertToDecimal(event.params.reward)
+    slashedEvent.save()
+
+    // staker
+    staker.slashed = staker.slashed.plus(convertToDecimal(event.params.penalty))
     staker.save()
 }
 
 export function handleDeposited(event: Deposited): void {
+
+    // setup
     let staker = getOrCreateStaker(event.params.staker.toHex())
-    staker.deposited = staker.deposited + convertToDecimal(event.params.value)
+    let periodNumber = getPeriodNumber(event.block.timestamp.toI32())
+    getOrCreateTransaction(event.transaction, event.block)
+    let depositedEvent = new DepositedEvent(makeEventId(event.transaction.hash, event.logIndex))
+
+    // event base
+    depositedEvent.transaction = event.transaction.hash.toHex()
+    depositedEvent.timestamp = event.block.timestamp.toI32()
+    depositedEvent.period = periodNumber.toString()
+    depositedEvent.staker = staker.id
+
+    // event record
+    depositedEvent.value = convertToDecimal(event.params.value)
+    depositedEvent.duration = BigInt.fromI32(event.params.periods)
+    depositedEvent.save()
+
+    //staker
+    staker.deposited = staker.deposited.plus(convertToDecimal(event.params.value))
     staker.save()
 }
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,45 +1,59 @@
-import {Staker} from "../generated/schema";
-import {BigInt} from "@graphprotocol/graph-ts/index";
-import {Address, BigDecimal} from "@graphprotocol/graph-ts";
+import {Period, Staker, Transaction} from "../generated/schema";
+import {Address, BigDecimal, BigInt, Bytes, dataSource, ethereum} from "@graphprotocol/graph-ts";
+import {StakingEscrow} from "../generated/StakingEscrow/StakingEscrow";
+
+export let MIGRATION_TIMESTAMP = 1618358661;  // final genesis period block timestamp
+export let FINAL_GENESIS_PERIOD_NUMBER = BigInt.fromI32(18732);  // actual final genesis period number
+export let GENESIS_ERA_PERIOD_NUMBER = BigInt.fromI32(18000);  // estimated first genesis period number
+export let GENESIS_PERIOD_DURATION = 86400;  // seconds
+export let PERIOD_DURATION = 604800;  // seconds
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ZERO_BD = BigDecimal.fromString("0");
 export let BI_18 = BigInt.fromI32(18);
 export let ONE_BI = BigInt.fromI32(1);
 export let NULL_ADDRESS = Address.fromString(
-  "0000000000000000000000000000000000000000"
+    "0000000000000000000000000000000000000000"
 );
 
 
+export function makeEventId(hash: Bytes, index: BigInt): string {
+  return hash.toHex().concat("-").concat(index.toString());
+}
+
+
 export function getNuCypherTokenAddress(network: string): string {
-  if (network == "mainnet") {
-    return "4fE83213D56308330EC302a8BD641f1d0113A4Cc";
-  } else if (network == "rinkeby") {
+  if (network == "rinkeby") {
     return "6A6F917a3FF3d33d26BB4743140F205486cD6B4B";
   } else if (network == "goerli") {
     return "02B50E38E5872068F325B1A7ca94D90ce2bfff63";
+  } else {
+      // mainnet
+      return "4fE83213D56308330EC302a8BD641f1d0113A4Cc";
   }
 }
 
 
 export function getStakingEscrowAddress(network: string): string {
-  if (network == "mainnet") {
-    return "bbD3C0C794F40c4f993B03F65343aCC6fcfCb2e2";
-  } else if (network == "rinkeby") {
+  if (network == "rinkeby") {
     return "6A6F917a3FF3d33d26BB4743140F205486cD6B4B";
   } else if (network == "goerli") {
     return "40Ca356d8180Ddc21C82263F9EbCeaAc6Cad7250";
+  } else {
+    // mainnet
+    return "bbD3C0C794F40c4f993B03F65343aCC6fcfCb2e2";
   }
 }
 
 
 export function getPolicyManagerAddress(network: string): string {
-  if (network == "mainnet") {
-    return "67E4A942c067Ff25cE7705B69C318cA2Dfa54D64";
-  } else if (network == "rinkeby") {
+  if (network == "rinkeby") {
     return "4db603E42E6798Ac534853AA2c0fF5618cb0ebE5";
   } else if (network == "goerli") {
     return "aC5e34d3FD41809873968c349d1194D23045b9D2";
+  } else {
+    // mainnet
+    return "67E4A942c067Ff25cE7705B69C318cA2Dfa54D64";
   }
 }
 
@@ -72,9 +86,66 @@ export function getOrCreateStaker(id: string): Staker {
         staker.substakes = ZERO_BI
         staker.minted = ZERO_BD
         staker.withdrawn = ZERO_BD
-        staker.commitment = ZERO_BI
+        staker.lastCommitment = ZERO_BI
         staker.migrated = false
+        staker.snapshots = true
         staker.save()
     }
     return staker as Staker;
+}
+
+
+export function getOrCreateTransaction(transaction: ethereum.Transaction, block: ethereum.Block): Transaction {
+  let tx = Transaction.load(transaction.hash.toHex())
+  if (tx == null) {
+    let tx = new Transaction(transaction.hash.toHex());
+    tx.blockNumber = block.number;
+    tx.gasUsed = transaction.gasUsed;
+    tx.gasPrice = transaction.gasPrice;
+    tx.timestamp = block.timestamp.toI32();
+    tx.from = transaction.from.toHex();
+    tx.to = transaction.to.toHex();
+    tx.save();
+  }
+  return tx as Transaction
+}
+
+
+export function getPeriodNumber(timestamp: i32): BigInt {
+    let periodNumber: BigInt;
+    if (timestamp < MIGRATION_TIMESTAMP) {
+        periodNumber = BigInt.fromI32(timestamp / GENESIS_PERIOD_DURATION)
+    } else {
+        periodNumber = BigInt.fromI32(timestamp / PERIOD_DURATION);
+    }
+    return periodNumber;
+}
+
+export function finalizePeriod(periodNumber: i32): void {
+    let period = Period.load(periodNumber.toString())
+    if (period != null) {
+        let stakingEscrowAddress = Address.fromString(getStakingEscrowAddress(dataSource.network()))
+        let contract = StakingEscrow.bind(stakingEscrowAddress)
+        period.circulatingSupply = convertToDecimal(contract.previousPeriodSupply())
+        if (period.totalStaked > ZERO_BD) {
+            period.participationRate = period.totalStaked.div(period.circulatingSupply);
+        }
+        period.finalized = true;
+        period.save()
+    }
+}
+
+
+export function createPeriod(periodNumber: BigInt, timestamp: i32): Period {
+    let period = new Period(periodNumber.toString())
+    period.timestamp = timestamp
+    period.activeStakers = ZERO_BI
+    period.totalStaked = ZERO_BD
+    period.circulatingSupply = ZERO_BD
+    period.participationRate = ZERO_BD
+    period.minted = ZERO_BD
+    period.finalized = false
+    period.genesis = periodNumber > GENESIS_ERA_PERIOD_NUMBER;
+    period.save()
+    return period as Period
 }
